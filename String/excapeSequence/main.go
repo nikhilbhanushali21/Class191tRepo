5be8c8a59816f22f17671079ae01c997b4eb9f49
nikhilbhanushali21 <nikhilbhanushali21@gmail.com>
2015-12-02 03:45:18 -0800

    program that excape sequence updates

--- String/excapeSequence/main.go
+++ String/excapeSequence/main.go
@@ -2,10 +2,6 @@ package main
 
 import "fmt"
 
-type mySentence string
-
 func main() {
-	var message mySentence = "Hello World!"
-	fmt.Println(message)
-	fmt.Printf("%T\n", message)
+fmt.Println("Hello\tWorld\nHow are you?")
 }
